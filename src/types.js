9738cbb1f8848710105009b345f1b56f2d738f3b
Alejandro Arco <alejandroarpu@gmail.com>
2025-06-26 17:31:41 +0200

    fix: round emojis as imports

--- src/types.js
+++ src/types.js
@@ -1,17 +1,29 @@
+import zero from "./assets/numbers/zero.avif";
+import one from "./assets/numbers/one.avif";
+import two from "./assets/numbers/two.avif";
+import three from "./assets/numbers/three.avif";
+import four from "./assets/numbers/four.avif";
+import five from "./assets/numbers/five.avif";
+import six from "./assets/numbers/six.avif";
+import seven from "./assets/numbers/seven.avif";
+import eight from "./assets/numbers/eight.avif";
+import nine from "./assets/numbers/nine.avif";
+import ten from "./assets/numbers/keycap_ten.avif";
+
 export const gradients = ["gradient1", "gradient2", "gradient3"];
 
 export const modes = { Easy: 5, Medium: 7, Hard: 10 };
 
 export const roundsEmojis = [
-  "/src/assets/numbers/zero.avif",
-  "/src/assets/numbers/one.avif",
-  "/src/assets/numbers/two.avif",
-  "/src/assets/numbers/three.avif",
-  "/src/assets/numbers/four.avif",
-  "/src/assets/numbers/five.avif",
-  "/src/assets/numbers/six.avif",
-  "/src/assets/numbers/seven.avif",
-  "/src/assets/numbers/eight.avif",
-  "/src/assets/numbers/nine.avif",
-  "/src/assets/numbers/ten.avif",
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
 ];
